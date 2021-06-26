e3f83303bb2acac09a637b67bf2cecaca536db0f
haoliwei <haoliweiwork@gmail.com>
2021-06-26 15:14:18 +0800

    Version: 0.1.2 [+]配置 glfw glad glm 库

---
+++ include/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Von/library/glm-0.9.9.8/glm
+# Build directory: C:/Users/Von/library/glm-0.9.9.8/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glad/glad.h>
 
 int main(int argc, char *argv[])
 {
+	glm::vec3 camera = glm::vec3(0.1f, 0.0f, 0.0f);
 	std::cout << "Hello world!" << std::endl;
 }
